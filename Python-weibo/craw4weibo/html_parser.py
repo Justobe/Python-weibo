b2a2c97fbaff19503d6e63df86cde7d8bb631cd7
Justobe <18009677822@163.com>
2017-05-05 11:39:34 +0800

    the source code of blog

---
+++ Python-weibo/craw4weibo/html_parser.py
@@ -0,0 +1,134 @@
+import sys
+import os
+import requests
+import time
+from lxml import etree
+
+
+class HtmlParser(object):
+
+    def _save_user_data(self, url):
+        print url
+        cookie = "#Your Cookie"
+        headers = "#Your header"
+        #get web page using request
+        response = requests.get(url, cookies=cookie, headers=headers)
+        print "code:", response.status_code
+        html_cont = response.content
+        # get current user_id
+        res_url = url.split("/")
+        num = len(res_url)
+        user_id = res_url[num - 1]
+        #print user_id
+        selector = etree.HTML(html_cont)
+        #print selector.xpath('//input[@name="mp"]')
+        #get page of the weibo
+        pageNum = (int)(selector.xpath('//input[@name="mp"]')[0].attrib['value'])
+        print "page",pageNum
+        result = ""
+        word_count = 1
+        times = 5
+        one_step = pageNum / times
+        for step in range(times):
+            if step < times - 1:
+                i = step * one_step + 1
+                j = (step + 1) * one_step + 1
+            else:
+                i = step * one_step + 1
+                j = pageNum + 1
+            for page in range(i, j):
+                try:
+                    #download weibo of certain page
+                    url = 'https://weibo.cn/%s?filter=1&page=%d' % (user_id, page)
+                    #print url
+                    lxml = requests.get(url, cookies=cookie, headers=headers).content
+                    selector = etree.HTML(lxml)
+                    content = selector.xpath('//span[@class="ctt"]')
+                    for each in content:
+                        text = each.xpath('string(.)')
+                        #print text
+                        if word_count >= 3:
+                            text = "%d: " % (word_count - 2) + text + "\n"
+                        else:
+                            text = text + "\n\n"
+                        result = result + text
+                        word_count += 1
+                    print 'getting',page, ' page word ok!'
+                    sys.stdout.flush()
+
+                except:
+                    print page, 'error'
+                print page, 'sleep'
+                sys.stdout.flush()
+                time.sleep(3)
+            print 'continuing', step + 1, 'stopping'
+            time.sleep(10)
+
+        try:
+            #print "1"
+            file_name = "%s.txt" % user_id
+            fo = open(file_name, "wb")
+            #print "2"
+            fo.write(result.encode('utf-8'))
+            #print "3"
+            fo.close()
+            print 'finishing word spiderring'
+        except:
+            print 'cannot find adress'
+        sys.stdout.flush()
+
+    def _get_new_urls(self, url):
+        new_urls = set()
+        res_url = url.split("/")
+        num = len(res_url)
+        user_id = res_url[num - 1]
+        cookie = "#Your Cookie"
+        headers = "#Your header"
+        url = 'https://weibo.cn/%s/follow' % user_id
+        response = requests.get(url, cookies=cookie,headers=headers).content
+        selector = etree.HTML(response)
+        pageNum = (int)(selector.xpath('//input[@name="mp"]')[0].attrib['value'])
+
+        times = 5
+        one_step = pageNum / times
+        for step in range(times):
+            if step < times - 1:
+                i = step * one_step + 1
+                j = (step + 1) * one_step + 1
+            else:
+                i = step * one_step + 1
+                j = pageNum + 1
+            for page in range(i, j):
+                try:
+                    url = 'https://weibo.cn/%s/follow?page=%d' % (user_id, page)
+                    lxml = requests.get(url, cookies=cookie, headers=headers).content
+                    selector = etree.HTML(lxml)
+                    content = selector.xpath('/html/body/table/tr/td[2]/a[1]')
+                    for c in content:
+                        temp_url = c.attrib['href']
+                        if temp_url is not None:
+                            new_urls.add(temp_url)
+                    print 'geint follow',page, 'page word ok!'
+                    sys.stdout.flush()
+
+                except:
+                    print page, 'error'
+                print page, 'sleep'
+                sys.stdout.flush()
+                time.sleep(3)
+            print 'continuing', step + 1, 'stopping'
+            time.sleep(10)
+
+        return new_urls
+
+    def parse(self, url):
+
+        if url is None:
+            return
+        print "saving"
+        self._save_user_data(url)
+        print "getting"
+        new_urls = self._get_new_urls(url)
+        print "finishing"
+        return new_urls
+
---
+++ Python-weibo/craw4weibo/main.py
@@ -0,0 +1,32 @@
+from craw4weibo import html_parser,url_manager
+
+class SpiderMain(object):
+
+    def __init__(self):
+        self.urls = url_manager.UrlManager()
+        self.parser = html_parser.HtmlParser()
+
+    def craw(self, uid):
+        count = 1
+        root_url = 'https://weibo.cn/%s' % uid
+        self.urls.add_new_url(root_url)
+        while self.urls.has_new_url():
+            try:
+                new_url = self.urls.get_new_url()
+                print 'craw %d : %s' % (count, new_url)
+                new_urls = self.parser.parse(new_url)
+                self.urls.add_new_urls(new_urls)
+
+                if count == 1:
+                    break
+                count = count + 1
+            except:
+                print "craw failed"
+
+
+if __name__ == "__main__":
+    root_uid = "#Your root id"
+    spider = SpiderMain()
+    spider.craw(root_uid)
+
+
